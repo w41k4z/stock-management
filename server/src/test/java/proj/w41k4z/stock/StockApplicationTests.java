1f8950a15d346505e77d88604cb41a97add613cc
w41k4z <alainricor@gmail.com>
2023-11-13 04:10:35 +0300

    project init

---
+++ server/src/test/java/proj/w41k4z/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package proj.w41k4z.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
